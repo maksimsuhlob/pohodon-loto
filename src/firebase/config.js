c84e75b0acb91d076e5189ccfc461becb95ce118
maxim.suglob <maxim.suglob@solbeg.com>
2024-02-16 13:57:12 +0100

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD0bjw4z7AS98SYwa6rvAT7oUMWBGUaMJ0",
+    authDomain: "pohodon-loto.firebaseapp.com",
+    projectId: "pohodon-loto",
+    storageBucket: "pohodon-loto.appspot.com",
+    messagingSenderId: "270697512623",
+    appId: "1:270697512623:web:19eeaec6650538284e1e5a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
